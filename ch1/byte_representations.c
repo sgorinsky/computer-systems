89550710fe1c2bd8d224eab04a8cf3b7e7a4909d
Sam Gorinsky <sgorinsky@gmail.com>
2021-12-31 02:15:48 -0600

    exposition on show_bytes

--- ch1/byte_representations.c
+++ ch1/byte_representations.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
-typedef unsigned char *byte_pointer;
+typedef unsigned char* byte_pointer; // creates a new type that is a pointer to a variable
 
-void show_bytes(byte_pointer start, size_t len) { 
-    int i;
-    for (i=0;i<len;i++) {
-        printf(" %.2x", start[i]);
+void show_bytes(byte_pointer start, size_t len) { // pointer to array of start bytes
+    for (int i = 0; i < len; i++) {
+        printf(" %.2x", start[i]); // displays 2 digit hex representation of address of ith index of bytes array
     }
     printf("\n");
 }
 
 void show_int(int x) {
-    show_bytes((byte_pointer) &x, sizeof(int));
+    show_bytes((unsigned char*) &x, sizeof(int));
 }
 void show_float(float x) {
     show_bytes((byte_pointer) &x, sizeof(float));
 }
 void show_pointer(void *x) {
-    show_bytes((byte_pointer) &x, sizeof(void *));
+    show_bytes((unsigned char*) &x, sizeof(void *));
 }
 
 void test_show_bytes(int val) {
@@ -28,8 +27,7 @@ void test_show_bytes(int val) {
     show_pointer(pval);
 }
 
-int main()
-{
+int main() {
     test_show_bytes(12345);
     return 0;
 }
