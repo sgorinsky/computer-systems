6f41dbff93ae05d22142e51b2b7f9136df356457
Sam Gorinsky <sgorinsky@gmail.com>
2022-01-02 21:25:53 -0600

    signed/twos-complement and unsigned practice

---
+++ 2 - Representing and Manipulating Information/2.2 - Integer Representations/integer_representations.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/* Practice 2.17: Convert between Hexadecimal, binary, B2U, B2T
+
+Hexadecimal Binary B2U B2T
+
+0xA         1010   10  -6
+0x1         0001   1   1
+0xB         1011   11  -5
+0x2         0010   2   2
+0x7         0111   7   7
+0xC         1100   12  -4
+*/
+
+/* Practice 2.18: T2U of 4-bit integers
+
+x  T2U
+
+-1  15
+-5  11  
+-6  10
+-4  12
+1   1
+8   8
+*/
+
