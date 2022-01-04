0ed008d0afbd2727a4605508e592c700a7617492
Sam Gorinsky <sgorinsky@gmail.com>
2022-01-03 22:28:09 -0600

    predicate can add two unsigned ints w/o overflow?

--- 2/2.3/integer_arithmetic.c
+++ 2/2.3/integer_arithmetic.c
@@ -1,1 +1,14 @@
 #include <stdio.h>
+
+// Problem 2.27: Determine if two unsigned ints can be added without overflow
+int uadd_ok(unsigned int x, unsigned int y) {
+    unsigned int difference = __UINT32_MAX__ - x;
+    return y <= difference;
+}
+
+int main() {
+    unsigned int x = 0xFF000000;
+    printf("can add %d\n", uadd_ok(0xFFFFFF00, 0xFF));
+    
+    return 0;
+}
