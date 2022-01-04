63d7966d4e69f10dbe4c845ae1c66b8283321a57
Sam Gorinsky <sgorinsky@gmail.com>
2022-01-03 22:33:31 -0600

    better logging for uadd_ok predicate

--- 2/2.3/integer_arithmetic.c
+++ 2/2.3/integer_arithmetic.c
@@ -2,13 +2,14 @@
 
 // Problem 2.27: Determine if two unsigned ints can be added without overflow
 int uadd_ok(unsigned int x, unsigned int y) {
-    unsigned int difference = __UINT32_MAX__ - x;
+    unsigned int difference = __UINT64_MAX__ - x;
     return y <= difference;
 }
 
 int main() {
     unsigned int x = 0xFF000000;
-    printf("can add %d\n", uadd_ok(0xFFFFFF00, 0xFF));
-    
+    printf("can we add %u + %u? %d\n", 0xFFFFFF00, 0xFF, uadd_ok(0xFFFFFF00, 0xFF));
+    printf("can we add %u + %u? %d\n", 0xFFFFFF00, 0xFFF, uadd_ok(0xFFFFFF00, 0xFFF));
+
     return 0;
 }
